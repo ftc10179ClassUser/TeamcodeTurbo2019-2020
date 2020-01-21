adaebda891255c30a008379e918b4421f958626b
Squirrles <techturtles10179@gmail.com>
2020-01-21 10:22:09 -0600

    commented near red triangle and near blue triangle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueTriangle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearBlueTriangle.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.lib.util.states.StartState;
 import org.firstinspires.ftc.teamcode.lib.util.states.State;
 
 @Autonomous(name="NearBlueTriangle")
-public class NearBlueTriangle extends AutonomousLibrary {
+public class NearBlueTriangle extends AutonomousLibrary {//Note: states are backwards, the one at the end runs first
     Servo foundationGrabber;
 
     @Override
@@ -17,40 +17,44 @@ public class NearBlueTriangle extends AutonomousLibrary {
         foundationGrabber.setPosition(0.5);
 
         State strafeRightToBridge = new StartState(() -> {
-            moveRightCentimeters(-120, -0.5);
+            moveRightCentimeters(-120, -0.5); //Strafe left 120cm at 0.5 speed to the bridge
         }, () -> true, () -> {}, "StrafeRightToBridge");
 
         State releasePlatform = new State(() -> {
-            foundationGrabber.setPosition(1);
+            foundationGrabber.setPosition(1); //Open the foundation grabber servo
             return false;
-        }, () -> {
-            stateMachine.addState(strafeRightToBridge);
+        }, () -> { //When the state is done
+            stateMachine.addState(strafeRightToBridge); //Run strafeRightToBridge
         }, 1000, "ReleasePlatform");
 
         State moveBackwardFromPlatform = new StartState(() -> {
+            //Move backward 110cm at 0.5 speed to the loading zone, and run releasePlatform afterward
             moveForwardCentimeters(110, 0.5, releasePlatform);
         }, () -> true, () -> {},"MoveBackwardFromPlatform");
 
         State grabPlatform = new State(() -> {
-            foundationGrabber.setPosition(0);
+            foundationGrabber.setPosition(0); //Close the foundation grabber servo
             return false;
-        }, () -> {
-            stateMachine.addState(moveBackwardFromPlatform);
+        }, () -> { //When the state is done
+            stateMachine.addState(moveBackwardFromPlatform); //Run moveBackwardFromPlatform
         }, 1000, "GrabPlatform");
 
         State moveForwardToPlatform = new StartState(() -> {
+            //Move forward 70cm at 0.5 speed to the foundation, and run grabPlatform afterward
             moveForwardCentimeters(-70, -0.5, grabPlatform);
         }, () -> true, () -> {}, "MoveForwardToPlatform");
 
         State strafeAlign = new StartState(() -> {
+            //Align the bot with the foundation where the foundation will stay in the loading zone while the bot navigates
             wheelController.moveXY(0, 0.1);
             moveRightCentimeters(40.5, 0.5, moveForwardToPlatform);
         }, () -> true, () -> {}, "StrafeAlign");
 
         State moveOffWall = new StartState(() -> {
+            //Move forward 8cm at 0.5 speed to avoid future friction with the wall, and run strafeAlign afterward
             moveForwardCentimeters(-8, -0.5, strafeAlign);
         }, () -> true, () -> {}, "MoveForwardToPlatform");
 
-        stateMachine.addState(moveOffWall);
+        stateMachine.addState(moveOffWall);//After we set up the states, we add the first one to our state machine
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearRedTriangle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NearRedTriangle.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.lib.util.states.StartState;
 import org.firstinspires.ftc.teamcode.lib.util.states.State;
 
 @Autonomous(name="NearRedTriangle")
-public class NearRedTriangle extends AutonomousLibrary {
+public class NearRedTriangle extends AutonomousLibrary {//Note: states are backwards, the one at the end runs first
     Servo foundationGrabber;
 
     @Override
@@ -16,46 +16,51 @@ public class NearRedTriangle extends AutonomousLibrary {
         foundationGrabber = getServo("foundationGrabber"); //Get the foundation grabber servo
         foundationGrabber.setPosition(1);
 
-        State strafeRightToBridge = new StartState(() -> {
-            moveRightCentimeters(120, 0.5); //Strafe  right 120cm at 0.5 speed, or to the bridge
+        State strafeLeftToBridge = new StartState(() -> {
+            moveRightCentimeters(120, 0.5); //Strafe right 120cm at 0.5 speed to the bridge
         }, () -> true, () -> {}, "StrafeRightToBridge");
 
         State releasePlatform = new State(() -> {
-            foundationGrabber.setPosition(1); //Pull up the foundation grabber servo
+            foundationGrabber.setPosition(1); //Open the foundation grabber servo
             return false;
         }, () -> { //When the state is done
-            stateMachine.addState(strafeRightToBridge); //Run this state
+            stateMachine.addState(strafeLeftToBridge); //Run strafeLeftToBridge
         }, 1000, "ReleasePlatform");
 
         State moveBackwardFromPlatform = new StartState(() -> {
+            //Move backward 110cm at 0.5 speed to the loading zone, and run releasePlatform afterward
             moveForwardCentimeters(110, 0.5, releasePlatform);
         }, () -> true, () -> {},"MoveBackwardFromPlatform");
 
         State grabPlatform = new State(() -> {
-            foundationGrabber.setPosition(0);
+            foundationGrabber.setPosition(0); //Close the foundation grabber servo
             return false;
-        }, () -> {
-            stateMachine.addState(moveBackwardFromPlatform);
+        }, () -> { //When the state is done
+            stateMachine.addState(moveBackwardFromPlatform); //Run moveBackwardFromPlatform
         }, 1000, "GrabPlatform");
 
         State moveForwardToPlatform = new StartState(() -> {
+            //Move forward 70cm at 0.5 speed to the foundation, and run grabPlatform afterward
             moveForwardCentimeters(-70, -0.5, grabPlatform);
         }, () -> true, () -> {}, "MoveForwardToPlatform");
 
         State strafeAlign = new StartState(() -> {
+            //Align the bot with the foundation where the foundation will stay in the loading zone while the bot navigates
             wheelController.moveXY(0, 0.1);
             moveRightCentimeters(-40.5, -0.5, moveForwardToPlatform);
         }, () -> true, () -> {}, "StrafeAlign");
 
         stateMachine.addState(new State(() -> {
+            //If motors are not moving, display the caption "Thingy" on the Driver Station
             telemetry.addData("Thingy", !getMotorsMoving());
             return false;
         }, () -> {}));
 
         State moveOffWall = new StartState(() -> {
+            //Move forward 8cm at 0.5 speed to avoid future friction with the wall, and run strafeAlign afterward
             moveForwardCentimeters(-8, -0.5, strafeAlign);
         }, () -> true, () -> {}, "MoveForwardToPlatform");
 
-        stateMachine.addState(moveOffWall);
+        stateMachine.addState(moveOffWall);//After we set up the states, we add the first one to our state machine
     }
 }
